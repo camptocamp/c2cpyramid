3e13daa5d00e03ed82348c28e3e38b1ed79b77a0
Bruno Binet <bruno.binet@camptocamp.com>
2011-05-18 14:44:50 +0000

    rename Session to DBSession to avoid confusion between sqlalchemy db session and the pyramid session object
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/c2cpyramid/trunk@910 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

--- c2cpyramid/template/+package+/models/__init__.py
+++ c2cpyramid/template/+package+/models/__init__.py
@@ -7,12 +7,12 @@ from papyrus.geo_interface import GeoInterface
 log = logging.getLogger(__name__)
 
 Base = sqlahelper.get_base()
-Session = sqlahelper.get_session()
+DBSession = sqlahelper.get_session()
 
 class Country(GeoInterface, Base):
     __tablename__ = 'countries'
     __table_args__ = {
         "autoload": True,
-        "autoload_with": Session.bind,
+        "autoload_with": DBSession.bind,
     }
     the_geom = GeometryColumn(Point(srid=4326))
